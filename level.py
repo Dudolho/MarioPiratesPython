0d3045eb27eb5411b267d6f48ded01cc0c57cf58
Dudolho <eduardobgrasel@gmail.com>
2023-12-11 23:17:10 -0300

    dia 1

---
+++ level.py
@@ -0,0 +1,23 @@
+import pygame
+from tiles import Tile
+from settings import tile_size
+
+class Level:
+    def __init__(self, level_data, surface):
+        self.display_surface = surface
+        self.setup_level(level_data)
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                if cell == 'X':
+                    x = col_index*64
+                    y = row_index*64
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+
+        
+
+    def run(self):
+        self.tiles.draw(self.display_surface)
---
+++ settings.py
@@ -0,0 +1,18 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX                         ',
+    ' XXXX        XX          XX ',
+    ' XXXX       XX              ',
+    ' XX    X  XXXX    XX  XX    ',
+    '       X  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    'XXXXXXXX  XXXXXX  XX  XXXX  ',
+]
+
+#dimensão da janela
+tile_size = 64
+screen_width = 1200
+screen_heigt = len(level_map)*tile_size
